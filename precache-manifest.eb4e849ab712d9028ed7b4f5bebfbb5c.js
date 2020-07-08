31bbd14b3fc206097cf4019641d669b7af39c59f
Abyssnya <68004671+Abyssnya@users.noreply.github.com>
2020-07-08 15:22:15 +0300

    Updates

---
+++ precache-manifest.eb4e849ab712d9028ed7b4f5bebfbb5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338b3486cc0e655217147121db27b954",
+    "url": "/Abyssnya/my-app/index.html"
+  },
+  {
+    "revision": "1385432f45398bbfba22",
+    "url": "/Abyssnya/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/Abyssnya/my-app/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Abyssnya/my-app/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1385432f45398bbfba22",
+    "url": "/Abyssnya/my-app/static/js/main.9ab0d7c4.chunk.js"
+  },
+  {
+    "revision": "36ebfc13333c0011205d",
+    "url": "/Abyssnya/my-app/static/js/runtime-main.b0a90011.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Abyssnya/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
